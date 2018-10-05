9fbf884c725769361210f275831254b9da176eaf
fmurtadho <fmurtadho@gmail.com>
2018-10-05 14:51:02 +0700

    jikan v2 fix yang hilang

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,13 @@
 const router = require('express').Router()
 const routerUser = require('./user')
 const routerMovie = require('./movie')
-// const routerJikan = require('./jikan')
+const routerJikan = require('./jikan')
 // const routerRandomAvatar = require('./randomAvatar')
 const routerDeezer = require('./deezer')
 
 router.use('/users', routerUser)
 router.use('/movies', routerMovie)
-// router.use('/jikan', routerJikan)
+router.use('/jikan', routerJikan)
 // router.use('/randomAvatar', routerRandomAvatar)
 router.use('/deezer', routerDeezer)
 
